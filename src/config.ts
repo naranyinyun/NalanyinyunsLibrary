247c229fbc64482d06b6602363b2f3458ed51413
naranyinyun <nalanyinyun@outlook.com>
2026-02-09 22:21:35 +0800

    rollback change

--- src/config.ts
+++ src/config.ts
@@ -65,8 +65,8 @@ export const profileConfig: ProfileConfig = {
 
 export const licenseConfig: LicenseConfig = {
 	enable: true,
-	name: "Nalanyinyun's Library Content License",
-	url: "https://github.com/naranyinyun/NalanyinyunsLibrary/blob/main/NLGL/",
+	name: "CC BY-NC-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
 };
 
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
