f4791dd9aea51cb752401e86d4b7a985e8032330
纳兰音韵 <91950505+naranyinyun@users.noreply.github.com>
2026-03-13 22:40:19 +0800

    fix: tell tailwind do not build unused class

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
-	darkMode: "class", // allows toggling dark mode manually
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
-	plugins: [require("@tailwindcss/typography")],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}",
+    "./src/styles/vendor/markdown.css",
+  ],
+  darkMode: "class", // allows toggling dark mode manually
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+  safelist: [
+    "copy-btn",
+    "copy-btn-icon",
+    "success-icon",
+    "line",
+    "expressive-code",
+    "anchor",
+    "anchor-icon",
+  ],
 };
