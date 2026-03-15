d7b56c2aeaba0d56dd60384c12c3466fd1fbdd54
naranyinyun <nalanyinyun@outlook.com>
2026-03-15 17:23:23 +0800

    fix: fix error file path

--- astro.config.mjs
+++ astro.config.mjs
@@ -118,12 +118,12 @@ export default defineConfig({
 				start_url: "/",
 				icons: [
 					{
-						src: "/assets/images/192.png",
+						src: "/192.png",
 						sizes: "192x192",
 						type: "image/png",
 					},
 					{
-						src: "/assets/images/512.png",
+						src: "/512.png",
 						sizes: "512x512",
 						type: "image/png",
 					},
