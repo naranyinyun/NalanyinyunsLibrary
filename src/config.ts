31d9dddd449f57f7540a53a8d069c3621483c1d1
纳兰音韵 <91950505+naranyinyun@users.noreply.github.com>
2026-06-06 23:22:41 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Nalanyinyun's Library",
-	subtitle: "纳兰音韵的个人站点",
+	subtitle: "纳兰音韵的大图书馆",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 180, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
